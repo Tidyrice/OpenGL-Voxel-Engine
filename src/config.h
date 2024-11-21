de0203c5b2144259ae68ace0df19fd9549426946
Cheney Cai <cheneycai2004@hotmail.com>
2024-11-21 01:12:37 -0500

    open window

---
+++ src/config.h
@@ -0,0 +1,9 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define WINDOW_WIDTH 1280
+#define WINDOW_HEIGHT 720
+#define INITIAL_WINDOW_POSITION 200, 100 //x, y
+#define WINDOW_NAME "Procedural Generation"
+
+#endif // CONFIG_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
 #include <GL/freeglut.h>
 
+#include "config.h"
+
+static void RenderSceneCallback()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glutSwapBuffers();
+}
+
 int main (int argc, char* argv[]) {
     glutInit(&argc, argv);
-    
+    glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA|GLUT_DEPTH);
+
+    glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+    glutInitWindowPosition(INITIAL_WINDOW_POSITION);
+    int window = glutCreateWindow(WINDOW_NAME);
+    std::cout << "Window id: " << window << std::endl;
+
+    //clear all colours
+    GLclampf Red = 0.0f, Green = 0.0f, Blue = 0.0f, Alpha = 0.0f;
+    glClearColor(Red, Green, Blue, Alpha);
+
+    glutDisplayFunc(RenderSceneCallback);
+
+    glutMainLoop(); // never returns
+
     return 0;
 }
